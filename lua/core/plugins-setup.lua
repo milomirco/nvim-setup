49ae992afeccf8c759b5f6ffe562ba5925f45abe
milomirco <milomirco687@gmail.com>
2023-09-20 10:07:54 -0300

    se agrega tmux

--- lua/core/plugins-setup.lua
+++ lua/core/plugins-setup.lua
@@ -30,6 +30,9 @@ return require('packer').startup(function(use)
   use "nvim-tree/nvim-tree.lua"
   use "nvim-tree/nvim-web-devicons"
   use "nvim-treesitter/nvim-treesitter"
+  
+  -- pane movement plugin
+  use "christoomey/vim-tmux-navigator"
 
   
   -- telescope setup, fuzzy finder
