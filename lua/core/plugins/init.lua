702f0622f85ee9835a2cfe1b4aafdc5c2ee19294
milomirco <milomirco687@gmail.com>
2023-09-27 13:24:54 -0300

    treesitter

--- lua/core/plugins/init.lua
+++ lua/core/plugins/init.lua
@@ -3,10 +3,11 @@ require("core.plugins.telescope")
 require("core.plugins.theme")
 require("core.plugins.lualine")
 require("core.plugins.autopairs")
+require("core.plugins.treesitter")
 -- lsp imports
 require("core.plugins.lsp.mason")
 require("core.plugins.lsp.nvim-cmp")
 require("core.plugins.lsp.completion")
 require("core.plugins.lsp.lspconfig")
 require("core.plugins.lsp.lspsaga")
-require("core.plugins.lsp.lspsaga")
+
